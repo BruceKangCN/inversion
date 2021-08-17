54e16d7352cf8e70bdacec58389cce9f200073ec
BruceKangCN <brucekangcn@gmail.com>
2021-08-17 17:40:55 +0800

    add `get_y_axis_array` function

--- src/tests/test_util.py
+++ src/tests/test_util.py
@@ -13,5 +13,12 @@ class TestUtil(unittest.TestCase):
         arr = [l1, l2, l3, l4, l5]
         self.assertEqual(get_price_array(arr), [5.2, 7.8, 14.5, 68.2, 1.3])
 
+    def test_get_y_axis_array(self):
+        l1 = ((78,94), 14.99)
+        l2 = ((354, 108), 17.99)
+        l3 = ((532, 104), 9.99)
+        arr = [l1, l2, l3]
+        self.assertEqual(get_y_axis_array(arr), [94, 108, 104])
+
 if __name__ == '__main__':
     unittest.main()
--- src/util.py
+++ src/util.py
@@ -32,6 +32,29 @@ def get_price_array(array: List[Label]) -> List[float]:
     """
     return list(map(lambda label: label[1], array))
 
+def get_y_axis_array(array: List[Label]) -> List[int]:
+    """ convert label array to y axis array
+
+        ## Parameters
+
+        array: array of labels
+
+        ## Return
+
+        array of y axises
+
+        ## Example
+
+        ```
+        l1 = ((78,94), 14.99)
+        l2 = ((354, 108), 17.99)
+        labels = [l1, l2]
+
+        y_arr = get_y_axis_array(labels) # y_arr == [94, 108]
+        ```
+    """
+    return list(map(lambda label: label[0][1], array))
+
 # TODO implementation
 def sort_by_position(array: List[Label]):
     """ sort the given array of labels by position
